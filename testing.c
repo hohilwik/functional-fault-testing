1cc45c236a368a8c4393992fdf604c9fe5b8edc3
Shirley <80057848+hohilwik@users.noreply.github.com>
2022-10-09 21:00:51 +0530

    adding table

--- testing.c
+++ testing.c
@@ -106,3 +106,49 @@ int logic2( int param, int control, int a, int b, int c, int e, int h, int *y, i
 	return 0;
 	
 }
+
+int main()
+{
+	FILE *tb_file;
+	char buffer[8];
+	tb_file = fopen("testcases_exhaustive.txt", "r");
+	int o1, o2, v1, v2;
+	int in1, in2, in3, in4, in5;
+	
+	// iterating over test cases
+	for(int i=0; i<32; i++)
+	{	
+		fscanf(tb_file, "%s", buffer);
+		in1 = buffer[0]-48;
+		in2 = buffer[1]-48;
+		in3 = buffer[2]-48;
+		in4 = buffer[3]-48;
+		in5 = buffer[4]-48;
+		
+		printf("\nTestcase: %d\n", i+1);
+		
+		// iterating over nodes
+		for(int node=1; node<=16; node++)
+		{
+			// check for s-a-0 and s-a-1
+			for(int stuck=0; stuck<=1; stuck++)
+			{
+				logic2(node, stuck, in1, in2, in3, in4, in5, &o1, &o2);
+				logic2(0, stuck, in1, in2, in3, in4, in5, &v1, &v2);
+				
+				if( (o1!=v1)||(o2!=v2) )
+				{
+					printf("node: %d s-a-%d, ", node, stuck);
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+	
+	
+	return 0;
+}
+	
